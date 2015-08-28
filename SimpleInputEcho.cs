6646ef679a044a18ddbdce64fd8f1005df6517d5
Nico <info@nicoglas.com>
2015-08-28 14:05:45 +0200

    Forgot some semicolos

--- SimpleInputEcho.cs
+++ SimpleInputEcho.cs
@@ -2,8 +2,8 @@ using System;
 
 public class SimpleInputEcho{
 	public static void Main(){
-		Console.WriteLine("What's your name?")
-		Console.Write("Write your name here: ")
+		Console.WriteLine("What's your name?");
+		Console.Write("Write your name here: ");
 		var name = Console.ReadLine();
 		Console.WriteLine("Hello, {0}!", name);
 	}
