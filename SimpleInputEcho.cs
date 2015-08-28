aee9bdc473e645b1cb1ff6f63af2e0053ffbb052
Nico <info@nicoglas.com>
2015-08-28 14:05:23 +0200

    Modified SimpleInputEcho

--- SimpleInputEcho.cs
+++ SimpleInputEcho.cs
@@ -2,6 +2,9 @@ using System;
 
 public class SimpleInputEcho{
 	public static void Main(){
-		
+		Console.WriteLine("What's your name?")
+		Console.Write("Write your name here: ")
+		var name = Console.ReadLine();
+		Console.WriteLine("Hello, {0}!", name);
 	}
 }
