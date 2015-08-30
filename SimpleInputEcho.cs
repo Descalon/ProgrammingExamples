2406320f13f6f127fb29dac220beb4504b9bef6f
Nico <info@nicoglas.com>
2015-08-30 02:56:51 +0200

    Added static using

--- SimpleInputEcho.cs
+++ SimpleInputEcho.cs
@@ -1,10 +1,11 @@
 using System;
+using static System.Console;
 
 public class SimpleInputEcho{
 	public static void Main(){
-		Console.WriteLine("What's your name?");
-		Console.Write("Write your name here: ");
-		var name = Console.ReadLine();
-		Console.WriteLine($"Hello, {name}");
+		WriteLine("What's your name?");
+		Write("Write your name here: ");
+		var name = ReadLine();
+		WriteLine($"Hello, {name}");
 	}
 }
